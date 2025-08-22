fd35583de37243ceb4c738b7f5dbd43a55c01794
Anh Nguyen <nyameowmeow@icloud.com>
2025-08-22 10:59:56 +0700

    production-ready

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,37 @@ int errorCode = 0;
 bool setupTitleLogoReplacement(CCSprite* titleLogo)
 {
     auto limitLabelWidth = [](CCLabelBMFont* label, const float width, const float defaultScale, const float minScale) {
-        const auto originalWidth = label->getContentSize().width;
-        auto scale = label->getScale();
-        if (originalWidth > width && width > 0.0f) scale = width / originalWidth;
-        if (minScale != 0.0f && minScale >= scale) scale = minScale;
-        label->setScale(scale);
+        if (!label) return false;
+
+        const float originalWidth = label->getContentSize().width;
+        float currentScale = label->getScale();
+        float requested = (defaultScale > 0.0f) ? defaultScale : currentScale;
+
+        log::debug("[limitLabelWidth] originalWidth: {}, currentScale: {}, requested: {}",
+            originalWidth, currentScale, requested);
+
+        if (originalWidth <= 0.0f || width <= 0.0f) {
+            label->setScale(requested);
+            return true;
+        }
+
+        const float maxAllowed = width / originalWidth;
+        float finalScale = requested;
+
+        if (requested > 1.0f) {
+            if (originalWidth * requested <= width) finalScale = requested;
+            else finalScale = std::min(requested, maxAllowed);
+        } else {
+            if (originalWidth * requested <= width) finalScale = requested;
+            else finalScale = maxAllowed;
+        }
+
+        if (minScale > 0.0f && finalScale < minScale) finalScale = minScale;
+
+        label->setScale(finalScale);
+        return true;
     };
+    if (!titleLogo) return false;
 
     titleLogo->setOpacity(0);
 
@@ -27,22 +52,25 @@ bool setupTitleLogoReplacement(CCSprite* titleLogo)
     CCLabelBMFont* newTitleLogoUnderlay = CCLabelBMFont::create(temp.c_str(), "underlay_bigsheet.fnt"_spr);
     if (!newTitleLogo || !newTitleLogoUnderlay) return false;
 
-    const float predeterminedWidth = CCDirector::sharedDirector()->getWinSize().width * 0.69f;
+    const float predeterminedWidth = CCDirector::sharedDirector()->getWinSize().width * 0.75f;
+
     limitLabelWidth(newTitleLogo, predeterminedWidth, 1.25f, 0.25f);
+
     newTitleLogo->setID("custom-main-title"_spr);
     newTitleLogo->setZOrder(1);
     titleLogo->addChild(newTitleLogo);
 
     limitLabelWidth(newTitleLogoUnderlay, predeterminedWidth, 1.25f, 0.25f);
+
     newTitleLogoUnderlay->setID("custom-main-title-underlay"_spr);
     newTitleLogoUnderlay->setZOrder(0);
     titleLogo->addChild(newTitleLogoUnderlay);
+
     titleLogo->updateLayout();
 
     newTitleLogo->setPosition(titleLogo->getContentSize() / 2.f);
     newTitleLogo->setPosition(newTitleLogo->getPositionX() + 6.f, newTitleLogo->getPositionY() - 4.f);
     newTitleLogoUnderlay->setPosition(newTitleLogo->getPosition());
-
     return true;
 }
 
@@ -56,12 +84,12 @@ class $modify(TLTLoadingLayer, LoadingLayer)
     static void onModify(auto& self)
     {
         // darkmode "compatibility, thank you bitz"
-        if (Result<> res = self.setHookPriorityAfterPost("LoadingLayer::init", "bitz.darkmode_v4"); !res || res.isErr()) log::error("Failed to set hook priority: {}", res.unwrapErr());
+        if (Result<> res = self.setHookPriorityAfterPost("LoadingLayer::init", "bitz.darkmode_v4"); !res || res.isErr()) log::error("[TLTLoadingLayer::onModify] Failed to set hook priority: {}", res.unwrapErr());
+        // else log::debug("[TLTLoadingLayer::onModify] Successfully set hook priority after post for LoadingLayer::init");
     }
 
     $override bool init(const bool fromRefresh)
     {
-        // Initial setup - only on the first load
         errorCode = 0;
         std::string rawResponse = "ooh shiny";
         if (!fromRefresh)
@@ -74,42 +102,38 @@ class $modify(TLTLoadingLayer, LoadingLayer)
                 cachedEWDString = "Ruminative Dash";
             }
 
-            if (Mod::get()->getSettingValue<bool>("aggresive-prefetch"))
+            if (Mod::get()->getSettingValue<bool>("aggressive-prefetch"))
             {
                 auto req = web::WebRequest().timeout(std::chrono::seconds(3));
                 web::WebTask etask = req.get("https://raw.githubusercontent.com/AnhNguyenlost13/every-word-dash-api/refs/heads/master/badeline.txt");
-                // hog the game fr
                 while (etask.isPending()) std::this_thread::sleep_for(std::chrono::milliseconds(1));
                 if (rawResponse = etask.getFinishedValue()->string().unwrapOr("."); rawResponse == ".") errorCode += 1;
             } else
             {
                 // listener bind
-                m_fields->m_listener.bind([rawResponse] (web::WebTask::Event* event) mutable
-                    {
-                        if (const web::WebResponse* response = event->getValue()) rawResponse = response->string().unwrapOr(".");
-                        else if (event->isCancelled()) errorCode += 2;
-                    }
-                );
+                m_fields->m_listener.bind([&rawResponse] (web::WebTask::Event* event) mutable {
+                    if (const web::WebResponse* response = event->getValue()) rawResponse = response->string().unwrapOr(".");
+                    else if (event->isCancelled()) errorCode += 2;
+                });
 
                 // now we fetch (passively, don't hog the game)
                 m_fields->m_listener.setFilter(web::WebRequest().get("https://raw.githubusercontent.com/AnhNguyenlost13/every-word-dash-api/refs/heads/master/badeline.txt"));
                 if (rawResponse == "." || rawResponse.empty()) errorCode += 4;
-                else
-                {
-                    cachedEWDString = rawResponse;
-                    if (savedCache != cachedEWDString) Mod::get()->setSavedValue("cached-ewd-string", cachedEWDString);
-                }
+                else cachedEWDString = rawResponse;
             }
-            // listener already destroyed itself i think
-            // capitalize the string
+
+            if (rawResponse != cachedEWDString) Mod::get()->setSavedValue("cached-ewd-string", rawResponse);
+
             std::ranges::transform(cachedEWDString, cachedEWDString.begin(), [](const unsigned char c){ return std::toupper(c); });
 
             CCFileUtils::sharedFileUtils()->addSearchPath((Mod::get()->getTempDir() / "resources").string().c_str());
+
             CCTextureCache* textureCache = CCTextureCache::sharedTextureCache();
             (void) textureCache->addImage("merged_atlas.png"_spr, false);
             (void) textureCache->addImage("underlay_atlas.png"_spr, false);
+
             CCLabelBMFont::create("trans rights are human rights", "merged_output.fnt"_spr);
-            CCLabelBMFont::create("i should be more careful with bets next time", "underlay_bigsheet.fnt"_spr);
+            CCLabelBMFont::create("if you're curious why I have to create these labels, uhhh idk either lol rob does it so do I", "underlay_bigsheet.fnt"_spr);
         }
 
         if (Mod::get()->getSettingValue<bool>("every-word-dash-integration")) customTitleLogo = cachedEWDString;
@@ -117,14 +141,7 @@ class $modify(TLTLoadingLayer, LoadingLayer)
 
         if (!LoadingLayer::init(fromRefresh)) return false;
 
-        const auto titleLogo = typeinfo_cast<CCSprite*>(getChildByIDRecursive("gd-logo"));
-        if (!titleLogo)
-        {
-            log::error("node 'gd-logo' is missing for some reason?");
-            return true;
-        }
-
-        return setupTitleLogoReplacement(titleLogo);
+        if (const auto titleLogo = typeinfo_cast<CCSprite*>(getChildByIDRecursive("gd-logo")); !titleLogo || !setupTitleLogoReplacement(titleLogo)) return true;
     }
 };
 
@@ -133,10 +150,7 @@ class $modify(TLTMenuLayer, MenuLayer) {
         if (!MenuLayer::init()) return false;
 
         const auto titleLogo = typeinfo_cast<CCSprite*>(getChildByIDRecursive("main-title"));
-        if (!titleLogo) {
-            log::error("whoever removed the node with ID 'main-title' from the MenuLayer, you are a menace to society!"); // i'm keeping this
-            return true;
-        }
+        if (!titleLogo) return true;
 
         if (!Mod::get()->getSettingValue<bool>("every-word-dash-integration")) customTitleLogo = Mod::get()->getSettingValue<std::string>("custom-title-logo");
         else {
@@ -149,6 +163,6 @@ class $modify(TLTMenuLayer, MenuLayer) {
             else customTitleLogo = cachedEWDString;
         }
 
-        return setupTitleLogoReplacement(titleLogo);
+        if (!setupTitleLogoReplacement(titleLogo)) return true;
     }
 };
