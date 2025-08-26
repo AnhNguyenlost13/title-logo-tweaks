ec817e045d81e471e981ef7d035827132c2b446b
Anh Nguyen <nyameowmeow@icloud.com>
2025-08-26 09:20:06 +0700

    improve stuff, make the integration off by default ig

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,20 @@
 #include <Geode/ui/Notification.hpp>
 #include <Geode/utils/web.hpp>
 
+#include <mutex>
+#include <memory>
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <cctype>
+
 using namespace geode::prelude;
 
 std::string customTitleLogo = "Geometry Dash";
 std::string cachedEWDString = "Ruminative Dash";
-int errorCode = 0;
+std::atomic<int> errorCode{0};
+static std::mutex cachedMutex;
 
 bool setupTitleLogoReplacement(CCSprite* titleLogo)
 {
@@ -16,11 +25,8 @@ bool setupTitleLogoReplacement(CCSprite* titleLogo)
         if (!label) return false;
 
         const float originalWidth = label->getContentSize().width;
-        float currentScale = label->getScale();
-        float requested = (defaultScale > 0.0f) ? defaultScale : currentScale;
-
-        log::debug("[limitLabelWidth] originalWidth: {}, currentScale: {}, requested: {}",
-            originalWidth, currentScale, requested);
+        const float currentScale = label->getScale();
+        const float requested = (defaultScale > 0.0f) ? defaultScale : currentScale;
 
         if (originalWidth <= 0.0f || width <= 0.0f) {
             label->setScale(requested);
@@ -43,12 +49,18 @@ bool setupTitleLogoReplacement(CCSprite* titleLogo)
         label->setScale(finalScale);
         return true;
     };
+
     if (!titleLogo) return false;
 
     titleLogo->setOpacity(0);
 
-    std::string temp = customTitleLogo;
-    if (Mod::get()->getSettingValue<bool>("force-uppercase")) std::ranges::transform(temp, temp.begin(), [](const unsigned char c){ return std::toupper(c); });
+    std::string temp;
+    {
+        std::scoped_lock lock(cachedMutex);
+        temp = customTitleLogo;
+    }
+
+    if (Mod::get()->getSettingValue<bool>("force-uppercase")) std::ranges::transform(temp.begin(), temp.end(), temp.begin(), [](const unsigned char c){ return static_cast<char>(std::toupper(c)); });
 
     CCLabelBMFont* newTitleLogo = CCLabelBMFont::create(temp.c_str(), "merged_output.fnt"_spr);
     CCLabelBMFont* newTitleLogoUnderlay = CCLabelBMFont::create(temp.c_str(), "underlay_bigsheet.fnt"_spr);
@@ -73,6 +85,7 @@ bool setupTitleLogoReplacement(CCSprite* titleLogo)
     newTitleLogo->setPosition(titleLogo->getContentSize() / 2.f);
     newTitleLogo->setPosition(newTitleLogo->getPositionX() + 6.f, newTitleLogo->getPositionY() - 4.f);
     newTitleLogoUnderlay->setPosition(newTitleLogo->getPosition());
+
     return true;
 }
 
@@ -85,67 +98,111 @@ class $modify(TLTLoadingLayer, LoadingLayer)
 
     static void onModify(auto& self)
     {
-        // darkmode "compatibility, thank you bitz"
-        if (Result<> res = self.setHookPriorityAfterPost("LoadingLayer::init", "bitz.darkmode_v4"); !res || res.isErr()) log::error("[TLTLoadingLayer::onModify] Failed to set hook priority: {}", res.unwrapErr());
-        // else log::debug("[TLTLoadingLayer::onModify] Successfully set hook priority after post for LoadingLayer::init");
+        if (const Result<> res = self.setHookPriorityAfterPost("LoadingLayer::init", "bitz.darkmode_v4"); !res || res.isErr()) log::error("could not set prio: {}", res.unwrapErr());
     }
 
     $override bool init(const bool fromRefresh)
     {
-        errorCode = 0;
-        std::string rawResponse = "ooh shiny";
+        errorCode.store(0);
+
+        auto rawResponsePtr = std::make_shared<std::string>(".");
+
         if (!fromRefresh)
         {
-            // fetch the last string if the web req somehow didn't make it
             const auto savedCache = Mod::get()->getSavedValue<std::string>("cached-ewd-string");
-            cachedEWDString = savedCache;
-            if (savedCache.empty()) {
-                errorCode += 1;
-                cachedEWDString = "Ruminative Dash";
+            {
+                std::scoped_lock lock(cachedMutex);
+                if (!savedCache.empty()) cachedEWDString = savedCache;
             }
+
+            if (savedCache.empty()) errorCode.fetch_add(1);
+            else *rawResponsePtr = savedCache;
 
             if (Mod::get()->getSettingValue<bool>("aggressive-prefetch"))
             {
                 auto req = web::WebRequest().timeout(std::chrono::seconds(3));
                 web::WebTask etask = req.get("https://raw.githubusercontent.com/AnhNguyenlost13/every-word-dash-api/refs/heads/master/badeline.txt");
-                while (etask.isPending()) std::this_thread::sleep_for(std::chrono::milliseconds(1));
-                if (rawResponse = etask.getFinishedValue()->string().unwrapOr("."); rawResponse == ".") errorCode += 1;
-            } else
+
+                constexpr auto maxWait = std::chrono::milliseconds(3000);
+                auto waited = std::chrono::milliseconds(0);
+                constexpr auto step = std::chrono::milliseconds(1);
+                while (etask.isPending() && waited < maxWait) {
+                    std::this_thread::sleep_for(step);
+                    waited += step;
+                }
+
+                if (const auto finished = etask.getFinishedValue()) {
+                    *rawResponsePtr = finished->string().unwrapOr(".");
+                    if ((*rawResponsePtr) == ".")errorCode.fetch_add(1);
+                } else errorCode.fetch_add(1);
+            }
+            else
             {
-                // listener bind
-                m_fields->m_listener.bind([&rawResponse] (web::WebTask::Event* event) mutable {
-                    if (const web::WebResponse* response = event->getValue()) rawResponse = response->string().unwrapOr(".");
-                    else if (event->isCancelled()) errorCode += 2;
+                m_fields->m_listener.bind([rawResponsePtr] (web::WebTask::Event* event) mutable {
+                    if (const web::WebResponse* response = event->getValue()) {
+                        *rawResponsePtr = response->string().unwrapOr(".");
+
+                        if (!rawResponsePtr->empty() && *rawResponsePtr != ".") {
+                            std::scoped_lock lock(cachedMutex);
+                            cachedEWDString = *rawResponsePtr;
+                            Mod::get()->setSavedValue("cached-ewd-string", cachedEWDString);
+                        }
+                    }
+                    else if (event->isCancelled()) errorCode.fetch_add(2);
                 });
 
-                // now we fetch (passively, don't hog the game)
                 m_fields->m_listener.setFilter(web::WebRequest().get("https://raw.githubusercontent.com/AnhNguyenlost13/every-word-dash-api/refs/heads/master/badeline.txt"));
-                if (rawResponse == "." || rawResponse.empty()) errorCode += 4;
-                else cachedEWDString = rawResponse;
+
+                if ((*rawResponsePtr) == "." || rawResponsePtr->empty()) errorCode.fetch_add(4);
+                else {
+                    std::scoped_lock lock(cachedMutex);
+                    cachedEWDString = *rawResponsePtr;
+                }
             }
 
-            if (rawResponse != cachedEWDString) Mod::get()->setSavedValue("cached-ewd-string", rawResponse);
+            {
+                std::scoped_lock lock(cachedMutex);
+                if ((*rawResponsePtr) != cachedEWDString && !rawResponsePtr->empty() && (*rawResponsePtr) != ".") {
+                    Mod::get()->setSavedValue("cached-ewd-string", *rawResponsePtr);
+                    cachedEWDString = *rawResponsePtr;
+                }
+            }
 
-            std::ranges::transform(cachedEWDString, cachedEWDString.begin(), [](const unsigned char c){ return std::toupper(c); });
+            {
+                std::string upper;
+                {
+                    std::scoped_lock lock(cachedMutex);
+                    upper = cachedEWDString;
+                }
+                std::ranges::transform(upper.begin(), upper.end(), upper.begin(), [](unsigned char c){ return static_cast<char>(std::toupper(c)); });
+                {
+                    std::scoped_lock lock(cachedMutex);
+                    cachedEWDString = std::move(upper);
+                }
+            }
 
             CCFileUtils::sharedFileUtils()->addSearchPath((Mod::get()->getTempDir() / "resources").string().c_str());
 
             CCTextureCache* textureCache = CCTextureCache::sharedTextureCache();
-            (void) textureCache->addImage("merged_atlas.png"_spr, false);
-            (void) textureCache->addImage("underlay_atlas.png"_spr, false);
+            auto tex1 = textureCache->addImage("merged_atlas.png"_spr, false);
+            auto tex2 = textureCache->addImage("underlay_atlas.png"_spr, false);
 
-            CCLabelBMFont::create("trans rights are human rights", "merged_output.fnt"_spr);
-            CCLabelBMFont::create("if you're curious why I have to create these labels, uhhh idk either lol rob does it so do I", "underlay_bigsheet.fnt"_spr);
+            auto labelA = CCLabelBMFont::create("trans rights are human rights", "merged_output.fnt"_spr);
+            auto labelB = CCLabelBMFont::create("if you're curious why I have to create these labels, uhhh idk either lol rob does it so do I", "underlay_bigsheet.fnt"_spr);
         }
 
-        if (Mod::get()->getSettingValue<bool>("every-word-dash-integration")) customTitleLogo = cachedEWDString;
+        if (Mod::get()->getSettingValue<bool>("every-word-dash-integration")) {
+            std::scoped_lock lock(cachedMutex);
+            customTitleLogo = cachedEWDString;
+        }
         else customTitleLogo = Mod::get()->getSettingValue<std::string>("custom-title-logo");
 
         if (!LoadingLayer::init(fromRefresh)) return false;
 
         const auto titleLogo = typeinfo_cast<CCSprite*>(getChildByIDRecursive("gd-logo"));
         if (!titleLogo) return true;
-        setupTitleLogoReplacement(titleLogo); return true;
+        setupTitleLogoReplacement(titleLogo);
+        return true;
     }
 };
 
@@ -159,17 +216,28 @@ class $modify(TLTMenuLayer, MenuLayer) {
         if (!Mod::get()->getSettingValue<bool>("every-word-dash-integration")) customTitleLogo = Mod::get()->getSettingValue<std::string>("custom-title-logo");
         else
         {
-            if (errorCode > 1) {
-            Notification::create(
-                "Failed to fetch/parse EWD string (errcode " + std::to_string(errorCode) + ")",
-                NotificationIcon::Error,
-                1.f
-            )->show();
-        }
-            else customTitleLogo = cachedEWDString;
+            if (errorCode.load() > 1) {
+                if (errorCode.load() == 5) {
+                    Notification::create(
+                    "Restart the game to load the latest string, sorry about that.",
+                    NotificationIcon::Info,
+                    1.f
+                    )->show();
+                } else {
+                    Notification::create(
+                    fmt::format("Failed to fetch/parse EWD string (errcode {})", errorCode.load()),
+                    NotificationIcon::Error,
+                    1.f
+                    )->show();
+                }
+            }
+            else {
+                std::scoped_lock lock(cachedMutex);
+                customTitleLogo = cachedEWDString;
+            }
         }
 
-        if (!setupTitleLogoReplacement(titleLogo)) return true;
+        setupTitleLogoReplacement(titleLogo);
         return true;
     }
 };
